63439a4bfb0c8a2e234845512a76b6af4b4c78d8
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2020-11-22 11:13:23 -0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,30 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="jumbotron mb-4">
         <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-sm-12 col-md-12">
-                    <div class="card border-0">
-                        @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                        @endif
+            <h3 class="font-weight-bold">Edwin Francisco Olivo Garcia</h3>
+            <h4>Universidad Autónoma de Nayarit</h4>
+            <p>Programación Distribuida del Lado del Servidor</p>
+            <a class="btn btn-lg btn-primary" href="{{ route('login') }}">{{ __('Login') }}</a>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-sm-12 col-md-12">
+                <div class="card border-0">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
                     </div>
-                    @foreach ($bromas as $broma) 
-                        <div class="card mb-4 border-0 shadow">
-                            <div class="card-body">
-                                <h5 class="card-title">Broma</h5>
-                                <p class="card-text">
-                                    {{ $broma->broma }}
-                                </p>
-                            </div>
-                        </div>
-                    @endforeach
-                    {{ $bromas->links() }}
+                    @endif
                 </div>
+                @foreach ($bromas as $broma) 
+                    <div class="card mb-4 border-0 shadow">
+                        <div class="card-body">
+                            <h5 class="card-title">Broma</h5>
+                            <p class="card-text">
+                                {{ $broma->broma }}
+                            </p>
+                        </div>
+                    </div>
+                @endforeach
+                {{ $bromas->links() }}
             </div>
         </div>
-    </body>
-</html>
+    </div>
 @endsection
