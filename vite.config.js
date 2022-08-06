0cdca3b24387af0806f64406da40c025f44874c3
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2022-08-06 12:07:47 -0600

    Create vite.config.js
    Vite config file

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
+        // react(),
+        // vue({
+        //     template: {
+        //         transformAssetUrls: {
+        //             base: null,
+        //             includeAbsolute: false,
+        //         },
+        //     },
+        // }),
+    ],
+});
