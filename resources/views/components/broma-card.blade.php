8be9df4a2fa0e4d45d9d6822a5c5060bb237304e
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2024-03-18 10:53:18 -0700

    Refactor broma-card.blade.php to move category buttons to the card footer

--- resources/views/components/broma-card.blade.php
+++ resources/views/components/broma-card.blade.php
@@ -3,14 +3,16 @@
         <div class="card-body">
             <p class="card-text">{{ $broma->broma }}</p>
             <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                    @forelse ($broma->categorias as $elemento)
-                    <button type="button" class="btn btn-sm btn-outline-secondary">{{ $elemento->categoria }}</button>
-                    @empty
-                    <div>No category</div>
-                    @endforelse
-                </div>
                 <small class="text-muted">{{ $broma->fecha }}</small>
+            </div>
+        </div>
+        <div class="card-footer">
+            <div class="btn-group">
+                @forelse ($broma->categorias as $elemento)
+                <button type="button" class="btn btn-sm btn-outline-secondary">{{ $elemento->categoria }}</button>
+                @empty
+                <div>No category</div>
+                @endforelse
             </div>
         </div>
     </div>
