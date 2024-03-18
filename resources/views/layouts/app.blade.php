bf3f320a52e1c9e64f9a0e3f0e838a3819011e88
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2024-03-18 10:44:38 -0700

    Fix import paths for Bootstrap and Vite configuration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Catálogo de bromas') }}</title>
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body class="d-flex flex-column h-100">
     <div id="app">
